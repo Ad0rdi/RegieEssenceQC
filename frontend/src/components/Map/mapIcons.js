f6c7fb78bcec429d3babc536315bc6b10d55c64a
Adam <adam@example.com>
2026-04-20 16:40:42 -0400

    refactor: remove duplicate defaultIcon, fix import order in mapIcons

--- frontend/src/components/Map/mapIcons.js
+++ frontend/src/components/Map/mapIcons.js
@@ -1,15 +1,6 @@
+import L from 'leaflet';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-import L from 'leaflet';
-
-const defaultIcon = L.icon({
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [25, 41],
-});
 
 const selectedIcon = L.icon({
   iconUrl: markerIcon,
@@ -20,4 +11,4 @@ const selectedIcon = L.icon({
   shadowSize: [25, 41],
 });
 
-export { defaultIcon, selectedIcon };
+export { selectedIcon };
